02d1e09de4ab12aaa2cbfcb1c667de0ae50333cd
Tim Gates <tim.gates@iress.com>
2019-06-12 20:17:54 +1000

    Run another module using runpy

--- app/pyruncompare/__main__.py
+++ app/pyruncompare/__main__.py
@@ -1,16 +1,50 @@
 """
 Module load handler for execution via python -m pyruncompare.
+
+Usage:
+    %(exename)s [options] [<args>...]
+    %(exename)s (-h | --help)
+
+Options:
+    -h --help         Show this screen
+    -m <module>       Import and run specified module like python -m
+
 """
 from __future__ import (
     absolute_import, print_function, division, unicode_literals,
 )
 
 
+# {{{ Imports
+# System Imports
+import sys
+import runpy
+# External Imports
+from docopt import docopt
+# }}}
+
+
 def main():
     """
-    Test
+    Main Command Line entry point
     """
-    print('Done.')
+    args = docopt(__doc__ % {
+        'exename': ''.join(sys.argv[0:1]),
+    })
+    if args['-m']:
+        log_module_run(args['-m'], args)
+    else:
+        print('Unknown options: %r' % (args,))
+
+
+def log_module_run(modulename, args):
+    """
+    Load and run the module like python -m with log dump enabled.
+    """
+    oldargs = list(sys.argv)
+    sys.argv = [modulename] + args['<args>']
+    runpy.run_module(modulename, run_name='__main__', alter_sys=True)
+    sys.argv = oldargs
 
 
 if __name__ == '__main__':
