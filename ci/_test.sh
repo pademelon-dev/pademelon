29da16b503e3718c67757dde10fb9a910b33ae24
Tim Gates <tim.gates@iress.com>
2019-06-26 20:16:14 +1000

    Need to pass down host dir

--- ci/_test.sh
+++ ci/_test.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASEDIR="$( dirname "${THISDIR}" )"
+HOSTDIR="${1}"
+shift 1
 
 MAIN_MODULE="pademelon"
 MODULES=( "${MAIN_MODULE}" "test" )
@@ -14,6 +16,6 @@ cd "${BASEDIR}/app"
 python -m flake8 "${MODULES[@]}"
 python -m pylint "${MODULES[@]}"
 python -m bandit -r "${MAIN_MODULE}"
-python -m pytest --path-mapping "/workspace" "${BASEDIR}" --cov-report=term --cov-report=html --cov-config=.coveragerc --cov-fail-under=100 "--cov=${MAIN_MODULE}"
+python -m pytest --path-mapping "${BASEDIR}" "${HOSTDIR}" --cov-report=term --cov-report=html --cov-config=.coveragerc --cov-fail-under=100 "--cov=${MAIN_MODULE}"
 python -m pademelon --show-modified origin/master
 echo 'Testing Complete'
