4488e68a79ca72c084efa5d56206982090d1c51d
Tim Gates <tim.gates@iress.com>
2019-07-29 20:40:32 +1000

    Remove print debug

--- app/pademelon/changes.py
+++ app/pademelon/changes.py
@@ -49,11 +49,9 @@ def _get_modified(repo, basedir, branch):
     """
     Use git to get the list of modified files
     """
-    print(repr(branch))
     diff_entries = repo.commit(branch).diff('HEAD')
     for change_type in ('A', 'R', 'M'):
         for diff_entry in diff_entries.iter_change_type(change_type):
-            print(repr(diff_entry.change_type))
             fname = diff_entry.b_path
             fpath = os.path.join(basedir, fname)
             if os.path.isfile(fpath):
