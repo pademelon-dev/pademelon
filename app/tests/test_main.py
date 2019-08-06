d00cf69e22ed3fe531d3786d0af5dd62a158c9c5
Tim Gates <tim.gates@iress.com>
2019-08-06 17:05:58 +1000

    Make use of click for pademelon

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -6,42 +6,20 @@ Test modules for pademelon __main__
 def test_main():
     """
     GIVEN the pademelon.__main__ module entry point WHEN calling main THEN
-    the call executes successfully with a result of `None`
+    the call executes successfully with expected output
     """
     # Setup
     from pademelon.__main__ import main
+    from click.testing import CliRunner
     import mock
-    fake_docopt = mock.patch(
-        'pademelon.__main__.docopt', return_value={}
-    )
-    with fake_docopt:
-        # Exercise
-        result = main()  # pylint: disable=assignment-from-no-return
-    # Verify
-    assert result is None  # nosec
-
-
-def test_show_modified():
-    """
-    GIVEN the pademelon.__main__ module entry point WHEN calling main with the
-    --show-modified flag THEN the call executes successfully with a result of
-    `None`
-    """
-    # Setup
-    from pademelon.__main__ import main
-    import mock
-    fake_docopt = mock.patch(
-        'pademelon.__main__.docopt', return_value={
-            '--show-modified': 'origin/main',
-        }
-    )
     fake_get_modified = mock.patch(
         'pademelon.changes._get_modified', return_value=[
             'fake.txt',
         ]
     )
-    with fake_docopt, fake_get_modified:
+    runner = CliRunner()
+    with fake_get_modified:
         # Exercise
-        result = main()  # pylint: disable=assignment-from-no-return
+        result = runner.invoke(main, [])
     # Verify
-    assert result is None  # nosec
+    assert result.output == 'fake.txt\n'  # nosec
